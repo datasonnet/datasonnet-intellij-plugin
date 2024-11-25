3765e11331c8bcc2bbf5bd3f93cb0060623c94b2
Jacob Hoeflaken <jacob.hoeflaken@axians.com>
2024-11-25 15:17:35 +0100

    Fix for libraries not loading on Windows.

--- src/main/java/io/portx/datasonnet/config/DataSonnetProjectSettings.java
+++ src/main/java/io/portx/datasonnet/config/DataSonnetProjectSettings.java
@@ -6,10 +6,42 @@ import java.util.List;
 
 public class DataSonnetProjectSettings implements Serializable {
 
+    /**
+     * The default template for a new DataSonnet file.
+     */
+    private static final String DEFAULT_TEMPLATE = """
+            /** DataSonnet
+            version=2.5
+            default null
+            */
+            
+            {}
+            """;
+
+    private String defaultTemplate;
     private List<String> dataSonnetLibraryPaths;
 
     public DataSonnetProjectSettings() {
+        defaultTemplate = DEFAULT_TEMPLATE;
         dataSonnetLibraryPaths = new ArrayList<String>();
+    }
+
+    /**
+     * Get the default template for a new DataSonnet file.
+     *
+     * @return the default template
+     */
+    public String getDefaultTemplate() {
+        return defaultTemplate;
+    }
+
+    /**
+     * Set the default template for a new DataSonnet file.
+     *
+     * @param defaultTemplate the new default template
+     */
+    public void setDefaultTemplate(final String defaultTemplate) {
+        this.defaultTemplate = defaultTemplate;
     }
 
     public List<String> getDataSonnetLibraryPaths() {
